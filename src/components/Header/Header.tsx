7fb5c3d123acd27e5b659f322a76f8f0e3624539
chaehaeun <chae_3210@naver.com>
2023-08-09 09:02:25 +0900

    [🎨스타일] header 스타일 반응형 완성

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,34 @@ import { Link } from 'react-router-dom'
 import { BsYoutube } from 'react-icons/bs'
 import { Profile, SearchInput } from '@/components'
 import styles from './Header.module.scss'
-import { MdKeyboardVoice } from 'react-icons/md'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
+  const [isSearch, setIsSearch] = useState(false)
+
+  const handleSearch = () => {
+    setIsSearch(!isSearch)
+  }
+
+  // 화면 다시 커질 때 강제로 isSearch를 false로 만들어줌
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 640) {
+        setIsSearch(false)
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    // 컴포넌트 언마운트 시 리스너 제거
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isSearch ? styles.hide : ''}`}>
       <div>
         <button
           type="button"
@@ -24,10 +47,15 @@ const Header = () => {
           </Link>
         </h1>
       </div>
-      <SearchInput />
+      <SearchInput isShow={isSearch} setIsShow={setIsSearch} />
       <div>
-        <button type="button" className={styles.showSearch} aria-label="검색">
-          <MdKeyboardVoice aria-hidden />
+        <button
+          type="button"
+          onClick={handleSearch}
+          className={styles.showSearch}
+          aria-label="검색"
+        >
+          <AiOutlineSearch aria-hidden />
         </button>
         <Profile />
       </div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from './Header/Header'
 export { default as SearchInput } from './SearchInput/SearchInput'
+export { default as Profile } from './Profile/Profile'
