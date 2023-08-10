257580650cc3372fa33da79b47be0edd0a815602
chaehaeun <chae_3210@naver.com>
2023-08-10 16:43:37 +0900

    [🎨스타일] SideNav 마크업 작업(ing)

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,5 +1,15 @@
 import styles from './SideNav.module.scss'
 import { AiOutlineHome } from 'react-icons/ai'
+import { BsFillMoonFill } from 'react-icons/bs'
+import {
+  AiOutlineLike,
+  // AiFillLike
+} from 'react-icons/ai'
+import {
+  HiOutlineUser,
+  // HiUser
+} from 'react-icons/hi'
+import { IoLogOutOutline } from 'react-icons/io5'
 import { NavLink } from 'react-router-dom'
 
 interface SideNavProps {
@@ -8,48 +18,58 @@ interface SideNavProps {
 
 const SideNav = ({ children }: SideNavProps) => {
   return (
-    <>
-      <div>
-        <div className={styles.sideNav}>
-          <nav>
-            <div>
-              <ul>
-                <li>
-                  <NavLink to="/">
-                    <AiOutlineHome /> 홈
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/">
-                    <AiOutlineHome /> 마이페이지
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="playlists">
-                    <AiOutlineHome /> 좋아요 표시한 동영상
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <span>구독</span>
-              <ul>
-                <li></li>
-              </ul>
-            </div>
-            <div>
-              <ul></ul>
-            </div>
-          </nav>
+    <div className={styles.wrap}>
+      <div className={styles.sideNav}>
+        <nav>
           <ul>
-            <li>다크모드</li>
-            <li>로그아웃</li>
+            <li>
+              <NavLink to="/">
+                <AiOutlineHome aria-hidden />
+                <span>홈</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/">
+                <HiOutlineUser />
+                <span>마이페이지</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="playlists">
+                <AiOutlineLike />
+                <span>좋아요 표시한 동영상</span>
+              </NavLink>
+            </li>
           </ul>
-        </div>
-        <div className={styles.topKeyword}></div>
+          <div>
+            <span className={styles.subscribeList}>구독</span>
+            <ul>
+              <li>
+                <NavLink to="/">
+                  <div className={styles.channelProfile} />
+                  <span>채널명</span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <ul>
+          <li>
+            <button type="button">
+              <BsFillMoonFill aria-hidden />
+              <span>다크모드</span>
+            </button>
+          </li>
+          <li>
+            <button type="button">
+              <IoLogOutOutline aria-hidden />
+              <span>로그아웃</span>
+            </button>
+          </li>
+        </ul>
       </div>
-      <main>{children}</main>
-    </>
+      <main className={styles.main}>{children}</main>
+    </div>
   )
 }
 
