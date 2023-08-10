6d4e57fb5ae58fe0cc8835b6019fd6b5d2c2a7a2
chaehaeun <chae_3210@naver.com>
2023-08-10 17:46:47 +0900

    [🎨스타일] SideNav 마크업 작업(ing)

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,43 +1,92 @@
 import styles from './SideNav.module.scss'
-import { AiOutlineHome } from 'react-icons/ai'
-import { BsFillMoonFill } from 'react-icons/bs'
-import {
-  AiOutlineLike,
-  // AiFillLike
-} from 'react-icons/ai'
-import {
-  HiOutlineUser,
-  // HiUser
-} from 'react-icons/hi'
-import { IoLogOutOutline } from 'react-icons/io5'
+import { AiOutlineHome, AiFillHome } from 'react-icons/ai'
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs'
+import { AiOutlineLike, AiFillLike } from 'react-icons/ai'
+import { HiOutlineUser, HiUser } from 'react-icons/hi'
+import { RiLogoutBoxRLine } from 'react-icons/ri'
 import { NavLink } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
+import { darkmodeAtom } from '@/store'
 
 interface SideNavProps {
   children: React.ReactNode
 }
 
+// todo : 햄버거 누르면 사이드바 열리고 닫히는 기능
+// todo : 코드 리팩토링
+
 const SideNav = ({ children }: SideNavProps) => {
+  const [darkmode, setDarkmode] = useRecoilState(darkmodeAtom)
+
+  const toggleDarkmode = () => {
+    setDarkmode(!darkmode)
+  }
+
   return (
     <div className={styles.wrap}>
       <div className={styles.sideNav}>
         <nav>
           <ul>
             <li>
-              <NavLink to="/">
-                <AiOutlineHome aria-hidden />
-                <span>홈</span>
+              <NavLink
+                to="/"
+                end
+                className={({ isActive }) => (isActive ? styles.active : '')}
+              >
+                {({ isActive }) => {
+                  return isActive ? (
+                    <>
+                      <AiFillHome aria-hidden />
+                      <span>홈</span>
+                    </>
+                  ) : (
+                    <>
+                      <AiOutlineHome aria-hidden />
+                      <span>홈</span>
+                    </>
+                  )
+                }}
               </NavLink>
             </li>
             <li>
-              <NavLink to="/">
-                <HiOutlineUser />
-                <span>마이페이지</span>
+              <NavLink
+                to="mypage"
+                end
+                className={({ isActive }) => (isActive ? styles.active : '')}
+              >
+                {({ isActive }) => {
+                  return isActive ? (
+                    <>
+                      <HiUser aria-hidden />
+                      <span>마이페이지</span>
+                    </>
+                  ) : (
+                    <>
+                      <HiOutlineUser aria-hidden />
+                      <span>마이페이지</span>
+                    </>
+                  )
+                }}
               </NavLink>
             </li>
             <li>
-              <NavLink to="playlists">
-                <AiOutlineLike />
-                <span>좋아요 표시한 동영상</span>
+              <NavLink
+                to="playlists"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+              >
+                {({ isActive }) => {
+                  return isActive ? (
+                    <>
+                      <AiFillLike aria-hidden />
+                      <span>좋아요 표시한 동영상</span>
+                    </>
+                  ) : (
+                    <>
+                      <AiOutlineLike aria-hidden />
+                      <span>좋아요 표시한 동영상</span>
+                    </>
+                  )
+                }}
               </NavLink>
             </li>
           </ul>
@@ -55,14 +104,23 @@ const SideNav = ({ children }: SideNavProps) => {
         </nav>
         <ul>
           <li>
-            <button type="button">
-              <BsFillMoonFill aria-hidden />
-              <span>다크모드</span>
+            <button type="button" onClick={toggleDarkmode}>
+              {darkmode ? (
+                <>
+                  <BsFillSunFill aria-hidden />
+                  <span>라이트모드</span>
+                </>
+              ) : (
+                <>
+                  <BsFillMoonFill aria-hidden />
+                  <span>다크모드</span>
+                </>
+              )}
             </button>
           </li>
           <li>
             <button type="button">
-              <IoLogOutOutline aria-hidden />
+              <RiLogoutBoxRLine aria-hidden />
               <span>로그아웃</span>
             </button>
           </li>
