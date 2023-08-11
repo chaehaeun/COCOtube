28620195d447760398142cb55877335785ce53cc
chaehaeun <chae_3210@naver.com>
2023-08-11 13:22:18 +0900

    [🎨스타일] 배너 이미지 비율 고정 스타일 추가

--- src/components/Channel/ChannelHeader/ChannelHeader.tsx
+++ src/components/Channel/ChannelHeader/ChannelHeader.tsx
@@ -10,7 +10,7 @@ const ChannelHeader = ({ type }: ChannelHeaderProps) => {
       <h3 className="sr-only_Title">{type} 헤더</h3>
       <div className={styles.headerImgWrap}>
         <img
-          src="https://yt3.googleusercontent.com/26Kpxnbd1W6qtZ2dfANyMafcZmcIjAS3riOR07F9acBUszAiXZZ9-HkCqIOjNtNrWd-CXyGicw=w1138-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
+          src="https://9to5mac.com/wp-content/uploads/sites/6/2021/09/Mac.png?w=1024"
           alt=""
         />
       </div>
