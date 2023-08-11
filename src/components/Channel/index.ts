93662e5bc497a3e5a64d699c050f5487762b1c07
chaehaeun <chae_3210@naver.com>
2023-08-11 16:33:04 +0900

    [🔨수정] export 수정

---
+++ src/components/Channel/index.ts
@@ -0,0 +1,5 @@
+export { default as Channel } from './Channel'
+export { default as ChannelHeader } from './ChannelHeader/ChannelHeader'
+export { default as ChannelInfo } from './ChannelInfo/ChannelInfo'
+export { default as ChannelBtn } from './ChannelInfo/ChannelBtn'
+export { default as ChannelNav } from './ChannelNav/ChannelNav'
