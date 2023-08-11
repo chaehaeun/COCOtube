f805b363c0a3d8496192d3ebe10a602ddc73e911
chaehaeun <chae_3210@naver.com>
2023-08-11 14:32:30 +0900

    [🎨스타일] 채널 인포 마크업 스타일링

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,8 +1,11 @@
+import { Channel } from '@/components'
+
 const MyPage = () => {
   // todo : 마이페이지 마크업 스타일링 끝내기
   return (
     <>
       <h2 className="sr-only_Title">MyPage</h2>
+      <Channel type="myPage" />
     </>
   )
 }
