239343ec0b0f451664db53ddeeff96ceb421596d
chaehaeun <chae_3210@naver.com>
2023-08-11 10:57:43 +0900

    [✨기능] VideoList 컴포넌트 분리

---
+++ src/components/VideoList/VideoList.tsx
@@ -0,0 +1,31 @@
+import styles from './VideoList.module.scss'
+import { VideoCard } from '@/components'
+
+const VideoList = () => {
+  return (
+    <ul className={styles.videoList}>
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+    </ul>
+  )
+}
+
+export default VideoList
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,11 @@
-import { VideoCard } from '@/components'
+import { VideoList } from '@/components'
 import styles from './Home.module.scss'
 
 const Home = () => {
   return (
     <>
       <h2 className={styles.title}>Home</h2>
-      <ul className={styles.videoList}>
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-      </ul>
+      <VideoList />
     </>
   )
 }
