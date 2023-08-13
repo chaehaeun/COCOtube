a9c730a3e3979ba50038aab774acd03d537987df
chaehaeun <chae_3210@naver.com>
2023-08-13 12:25:27 +0900

    [✨기능] SignIn 유효성 검사 로직 추가

--- src/components/Auth/AuthInput/AuthInput.tsx
+++ src/components/Auth/AuthInput/AuthInput.tsx
@@ -1,5 +1,66 @@
-const AuthInput = () => {
-  return <div></div>
+import styles from './AuthInput.module.scss'
+import { useState } from 'react'
+import { MdOutlineVisibility, MdOutlineVisibilityOff } from 'react-icons/md'
+import type {
+  FieldError,
+  FieldErrors,
+  UseFormRegisterReturn,
+} from 'react-hook-form'
+
+interface AuthInputProps {
+  label: string
+  type: 'text' | 'password' | 'email'
+  id: string
+  placeholder: string
+  register: UseFormRegisterReturn
+  errorMsg?: string | FieldError | FieldErrors | undefined
+}
+
+const AuthInput = ({
+  label,
+  type,
+  id,
+  placeholder,
+  register,
+  errorMsg,
+}: AuthInputProps) => {
+  const [isShowPassword, setIsShowPassword] = useState(false)
+  const inputType =
+    type === 'password' ? (isShowPassword ? 'text' : 'password') : type
+
+  const togglePasswordVisibility = () => {
+    setIsShowPassword(prev => !prev)
+  }
+
+  return (
+    <div className={styles.authInput}>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={inputType}
+        {...register}
+        id={id}
+        placeholder={placeholder}
+        className={type === 'password' ? styles.password : ''}
+      />
+      {type === 'password' && (
+        <button
+          onClick={togglePasswordVisibility}
+          type="button"
+          aria-label={`비밀번호 ${isShowPassword ? '숨기기' : '보이기'}`}
+          className={styles.passwordToggleBtn}
+        >
+          {isShowPassword ? (
+            <MdOutlineVisibilityOff />
+          ) : (
+            <MdOutlineVisibility />
+          )}
+        </button>
+      )}
+      {errorMsg && typeof errorMsg === 'string' && (
+        <p className={styles.errorMsg}>{errorMsg}</p>
+      )}
+    </div>
+  )
 }
 
 export default AuthInput
--- src/pages/AuthPages/SignIn.tsx
+++ src/pages/AuthPages/SignIn.tsx
@@ -1,7 +1,65 @@
+import { AuthInput, Modal } from '@/components'
+import styles from './AuthPage.module.scss'
+import { useModal } from '@/hooks'
+import { useForm } from 'react-hook-form'
+
 const SignIn = () => {
+  const { showModal, content, closeModal } = useModal()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+  })
+
+  const emailRegister = register('email', {
+    required: true,
+    pattern: {
+      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+      message: '유효한 이메일 형식이 아닙니다.',
+    },
+  })
+  const passwordRegister = register('password', {
+    required: true,
+    minLength: {
+      value: 8,
+      message: '비밀번호는 최소 8자 이상이어야 합니다.',
+    },
+  })
+
   return (
     <>
-      <h1 className="title_h1">로그인</h1>
+      <h1 className={styles.title}>로그인</h1>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit(entered => {
+          console.log(entered)
+        })}
+      >
+        <fieldset>
+          <legend className={styles.sronly}>로그인 폼</legend>
+          <AuthInput
+            register={emailRegister}
+            label="이메일"
+            type="email"
+            id="email"
+            placeholder="이메일을 입력해주세요."
+            errorMsg={errors.email?.message}
+          />
+          <AuthInput
+            register={passwordRegister}
+            label="비밀번호"
+            type="password"
+            id="password"
+            placeholder="비밀번호를 입력해주세요."
+            errorMsg={errors.password?.message}
+          />
+
+          <button type="submit">확인</button>
+        </fieldset>
+      </form>
+      {showModal && <Modal onClose={closeModal}>{content}</Modal>}
     </>
   )
 }
