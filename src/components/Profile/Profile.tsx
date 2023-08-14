716a25c07a92a5b42e3ae94ad1f5839ab0f37433
chaehaeun <chae_3210@naver.com>
2023-08-15 00:48:48 +0900

    [✨기능] 프로필사진 로딩 설정

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { authService } from '@/firebase-config'
+import MoonLoader from 'react-spinners/ClipLoader'
 import styles from './Profile.module.scss'
 
 interface UserData {
@@ -11,12 +12,14 @@ interface UserData {
 const Profile = () => {
   const navigate = useNavigate()
   const [userData, setUserData] = useState<UserData | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const unsubscribe = authService.onAuthStateChanged(user => {
       if (user) {
         setUserData({ photoURL: user.photoURL!, nickname: user.displayName! })
       }
+      setLoading(false)
     })
 
     return () => unsubscribe()
@@ -24,15 +27,21 @@ const Profile = () => {
 
   return (
     <>
-      {userData ? (
+      {loading ? (
+        <MoonLoader color="#4e1ac3" size={40} />
+      ) : userData ? (
         <button
           aria-label="프로필"
           type="button"
           className={styles.profile}
           onClick={() => navigate('/mypage')}
         >
-          <div>
-            <img src={userData.photoURL} alt={userData.nickname} />
+          <div className={styles.imageContainer}>
+            <img
+              src={userData.photoURL}
+              alt={userData.nickname}
+              className={styles.image}
+            />
           </div>
         </button>
       ) : (
