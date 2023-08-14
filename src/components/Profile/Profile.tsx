82b480679ed29de05fe8ebf3595a4609b63ab6de
chaehaeun <chae_3210@naver.com>
2023-08-15 00:27:11 +0900

    [✨기능] 프로필사진 설정

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,7 +1,47 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { authService } from '@/firebase-config'
 import styles from './Profile.module.scss'
 
+interface UserData {
+  nickname: string
+  photoURL: string
+}
+
 const Profile = () => {
-  return <button aria-label="프로필" type="button" className={styles.profile} />
+  const navigate = useNavigate()
+  const [userData, setUserData] = useState<UserData | null>(null)
+
+  useEffect(() => {
+    const unsubscribe = authService.onAuthStateChanged(user => {
+      if (user) {
+        setUserData({ photoURL: user.photoURL!, nickname: user.displayName! })
+      }
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  return (
+    <>
+      {userData ? (
+        <button
+          aria-label="프로필"
+          type="button"
+          className={styles.profile}
+          onClick={() => navigate('/mypage')}
+        >
+          <div>
+            <img src={userData.photoURL} alt={userData.nickname} />
+          </div>
+        </button>
+      ) : (
+        <Link to="signin" className={styles.signin}>
+          로그인
+        </Link>
+      )}
+    </>
+  )
 }
 
 export default Profile
