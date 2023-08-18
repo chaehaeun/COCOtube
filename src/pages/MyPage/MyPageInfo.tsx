574a42fcbe683b777e4cb1277bc7a7257f89575e
chaehaeun <chae_3210@naver.com>
2023-08-18 17:01:20 +0900

    [💩수정필요] 회원정보 페이지 마크업 ing

---
+++ src/pages/MyPage/MyPageInfo.tsx
@@ -0,0 +1,24 @@
+const MyPageInfo = () => {
+  // 뭐뭐 넣지
+  // 자기 소개
+  // 횐갑일
+  // 마지막 로그인
+
+  // 좋아요 눌러놓은 영상 수
+  // 구독 눌러놓은 채널 개수
+  // 댓글 작성 수
+  return (
+    <>
+      <h3 className="sr-only_Title">회원 상세 정보</h3>
+      <div>
+        <ul></ul>
+        <ul>
+          <li>통계</li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default MyPageInfo
