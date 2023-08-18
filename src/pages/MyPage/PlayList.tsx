45cc5e64303683c7c0cef566a78539ff211a36c8
chaehaeun <chae_3210@naver.com>
2023-08-18 17:00:46 +0900

    [🎨스타일] 플레이리스트 페이지 마크업 수정

--- src/pages/MyPage/PlayList.tsx
+++ src/pages/MyPage/PlayList.tsx
@@ -2,11 +2,11 @@ import { FilterBtnList, VideoList } from '@/components'
 
 const PlayList = () => {
   return (
-    <section>
-      <h4 className="sr-only_Title">PlayList</h4>
+    <>
+      <h3 className="sr-only_Title">PlayList</h3>
       <FilterBtnList />
       <VideoList />
-    </section>
+    </>
   )
 }
 
