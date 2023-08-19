08e86c293495d67f4593d5ff4255c25722c7e35a
chaehaeun <chae_3210@naver.com>
2023-08-19 09:43:58 +0900

    [🎨스타일] 회원 정보 마크업 수정

--- src/pages/MyPage/MyPageInfo.tsx
+++ src/pages/MyPage/MyPageInfo.tsx
@@ -1,21 +1,37 @@
 const MyPageInfo = () => {
-  // 뭐뭐 넣지
-  // 자기 소개
-  // 횐갑일
-  // 마지막 로그인
-
-  // 좋아요 눌러놓은 영상 수
-  // 구독 눌러놓은 채널 개수
-  // 댓글 작성 수
   return (
     <>
       <h3 className="sr-only_Title">회원 상세 정보</h3>
       <div>
-        <ul></ul>
         <ul>
-          <li>통계</li>
-          <li></li>
+          <li>
+            <span>자기 소개 말</span>
+            <p>안녕하세요. 잘 부탁드립니다.</p>
+          </li>
+          <li>
+            <span>좋아요 영상 개수</span>
+            <p>10개</p>
+          </li>
+          <li>
+            <span>구독 채널 개수</span>
+            <p>10개</p>
+          </li>
+          <li>
+            <span>작성한 댓글 수</span>
+            <p>10개</p>
+          </li>
         </ul>
+        <div>
+          <span>통계</span>
+          <ul>
+            <li>
+              가입일 : <span></span>
+            </li>
+            <li>
+              마지막 로그인 : <span></span>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   )
